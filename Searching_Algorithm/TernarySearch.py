521033875bdb0d9465838462b4e008d3b6844f01
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-08-23 05:52:21 +0200

    Update TernarySearch.py

--- Searching_Algorithm/TernarySearch.py
+++ Searching_Algorithm/TernarySearch.py
@@ -1,1 +1,3 @@
-
+'''
+Nel nostro esempio, la funzione f è una semplice parabola, e cerchiamo il punto di massimo all'interno dell'intervallo [0, 6]. Una volta trovato il punto di massimo, stampiamo il valore del punto e il valore massimo della funzione in quel punto.
+'''
