3c9453ef4025e0562df5c6abb835ff0598e1e1d2
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-08-24 06:49:48 +0100

    Update TernarySearch.py

--- Searching_Algorithm/TernarySearch.py
+++ Searching_Algorithm/TernarySearch.py
@@ -1,3 +1,12 @@
 '''
 Nel nostro esempio, la funzione f è una semplice parabola, e cerchiamo il punto di massimo all'interno dell'intervallo [0, 6]. Una volta trovato il punto di massimo, stampiamo il valore del punto e il valore massimo della funzione in quel punto.
 '''
+def ternary_search(f, l, r, eps):
+    while abs(r - l) > eps:
+        m1 = l + (r - l) / 3
+        m2 = r - (r - l) / 3
+        if f(m1) < f(m2):
+            l = m1
+        else:
+            r = m2
+    return (l + r) / 2
