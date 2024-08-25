f69632e53d684c85c989dabef284af52652d182c
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-08-25 06:54:05 +0100

    Update TernarySearch.py

--- Searching_Algorithm/TernarySearch.py
+++ Searching_Algorithm/TernarySearch.py
@@ -11,3 +11,12 @@ def ternary_search(f, l, r, eps):
         else:
             r = m2
     return (l + r) / 2
+def ternary_search(f, l, r, eps):
+    while abs(r - l) > eps:
+        m1 = l + (r - l) / 3
+        m2 = r - (r - l) / 3
+        if f(m1) < f(m2):
+            l = m1
+        else:
+            r = m2
+    return (l + r) / 2
