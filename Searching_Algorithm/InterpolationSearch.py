5a4b07c493e9a469e046c8c53d5a0513d317e047
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-08-29 07:54:17 +0100

    Update InterpolationSearch.py

--- Searching_Algorithm/InterpolationSearch.py
+++ Searching_Algorithm/InterpolationSearch.py
@@ -1,6 +1,6 @@
 def interpolation_search(arr, x):
     low = 0
     high = len(arr) - 1
-while low <= high and arr[low] <= x <= arr[high]:
+    while low <= high and arr[low] <= x <= arr[high]:
         # Calcola la stima della posizione dell'elemento
         pos = low + ((high - low) // (arr[high] - arr[low])) * (x - arr[low])
