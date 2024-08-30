79ec8ef20f6ea9f441b27364c7931c5822a95959
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-08-30 06:18:27 +0100

    Update InterpolationSearch.py

--- Searching_Algorithm/InterpolationSearch.py
+++ Searching_Algorithm/InterpolationSearch.py
@@ -7,3 +7,10 @@ def interpolation_search(arr, x):
    # Se l'elemento è stato trovato, restituisce il suo indice
         if arr[pos] == x:
             return pos
+           # Se l'elemento è maggiore, cerca a destra
+        if arr[pos] < x:
+            low = pos + 1
+        # Altrimenti, cerca a sinistra
+        else:
+            high = pos - 1
+            
