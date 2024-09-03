5f21eabf3b87a6d0bd48b72a84d2cd85084dc5da
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-09-03 09:19:46 +0200

    Update Exponential_Search.py

--- Searching_Algorithm/Exponential_Search.py
+++ Searching_Algorithm/Exponential_Search.py
@@ -2,3 +2,6 @@
 L'algoritmo di ricerca esponenziale è un metodo di ricerca per trovare un elemento in un array ordinato. 
 Funziona dividendo ripetutamente l'intervallo di ricerca a metà e controllando se l'elemento si trova nella metà inferiore o superiore.
 '''
+def ricerca_esponenziale(array, elemento, basso, alto):
+  "'
+  Funzione per implementare la ricerca esponenziale in un array ordinato.
