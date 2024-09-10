91a968d1f96508ebf46712e407af60d42b48ef13
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-09-10 08:09:04 +0200

    Update Ubiquitous_Binary_Search.py

--- Searching_Algorithm/Ubiquitous_Binary_Search.py
+++ Searching_Algorithm/Ubiquitous_Binary_Search.py
@@ -1,3 +1,14 @@
 '''
 E' simile standard binary search
 '''
+def binary_search(array, elemento):
+  """
+  Funzione per implementare la ricerca binaria in un array ordinato.
+
+  Argomenti:
+    array: l'array ordinato in cui cercare
+    elemento: l'elemento da cercare
+
+  Restituisce:
+    La posizione dell'elemento nell'array se trovato, altrimenti -1.
+  """
