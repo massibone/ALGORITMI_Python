788a41cb7446e9ca919082f129d76e0c293dd95c
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-09-10 08:09:32 +0200

    Update Ubiquitous_Binary_Search.py

--- Searching_Algorithm/Ubiquitous_Binary_Search.py
+++ Searching_Algorithm/Ubiquitous_Binary_Search.py
@@ -12,3 +12,38 @@ def binary_search(array, elemento):
   Restituisce:
     La posizione dell'elemento nell'array se trovato, altrimenti -1.
   """
+
+
+  basso = 0
+  alto = len(array) - 1
+  mezzo = 0
+
+  while basso <= alto:
+    # Calcola l'indice medio
+    mezzo = (basso + alto) // 2
+
+    # Controlla se l'elemento è stato trovato
+    if array[mezzo] == elemento:
+      return mezzo
+
+    # Controlla se l'elemento si trova nella metà inferiore
+    elif array[mezzo] > elemento:
+      alto = mezzo - 1
+
+    # Controlla se l'elemento si trova nella metà superiore
+    else:
+      basso = mezzo + 1
+
+  # Elemento non trovato
+  return -1
+
+# Esempio di utilizzo
+array = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
+elemento = 23
+
+risultato = binary_search(array, elemento)
+
+if risultato != -1:
+  print(f"L'elemento {elemento} è stato trovato in posizione {risultato}")
+else:
+  print(f"L'elemento {elemento} non è stato trovato nell'array")
