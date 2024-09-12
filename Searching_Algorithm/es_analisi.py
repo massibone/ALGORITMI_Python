7083611d787fa32943527f7f972c0bbcdfc3019a
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-09-12 08:04:28 +0200

    Update es_analisi.py

--- Searching_Algorithm/es_analisi.py
+++ Searching_Algorithm/es_analisi.py
@@ -1,3 +1,4 @@
 '''
-Data una funzione Python che legge un file di testo e restituisce una lista di parole ordinate per frequenza di apparizione, scomponi la funzione in sotto-funzioni che svolgono compiti specifici (es. lettura file, elaborazione testo, ordinamento).
+Data una funzione Python che legge un file di testo e restituisce una lista di parole ordinate per frequenza di apparizione, 
+scomponi la funzione in sotto-funzioni che svolgono compiti specifici (es. lettura file, elaborazione testo, ordinamento).
 '''
