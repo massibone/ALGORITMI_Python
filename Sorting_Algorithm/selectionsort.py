7e6f4c07fdd1ce5d882615af7105335314a01a00
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-09-17 08:04:10 +0200

    Update selectionsort.py

--- Sorting_Algorithm/selectionsort.py
+++ Sorting_Algorithm/selectionsort.py
@@ -1,1 +1,7 @@
+'''
+Il Selection Sort divide l'input in una parte ordinata e una parte non ordinata. Ad ogni iterazione, trova l'elemento minimo nella parte non ordinata e lo sposta nella parte ordinata.
 
+Entrambi questi algoritmi sono semplici da implementare ma non sono efficienti per grandi insiemi di dati. La loro complessità temporale è O(n^2) nel caso medio e nel caso peggiore, dove n è il numero di elementi nell'array.
+
+Per set di dati più grandi, algoritmi più efficienti come QuickSort, MergeSort o HeapSort sarebbero preferibili.
+'''
