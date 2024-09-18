07a5d3fdae7d1f227f52f1872ae654172fc1dd02
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-09-18 07:42:04 +0200

    Update selectionsort.py

--- Sorting_Algorithm/selectionsort.py
+++ Sorting_Algorithm/selectionsort.py
@@ -8,3 +8,24 @@ Per set di dati più grandi, algoritmi più efficienti come QuickSort, MergeSort
 '''
 Selection Sort: Questo algoritmo seleziona ripetutamente l'elemento minimo (o massimo) dalla parte non ordinata dell'array e lo sposta alla posizione corretta. È facile da implementare ma inefficiente per grandi quantità di dati, avendo una complessità temporale di O(n^2)
 '''
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+selectiotionsort2.py
+def selection_sort(arr):
+    n = len(arr)
+
+    # Scorrere tutti gli elementi dell'array
+    for i in range(n):
+        
+        # Trovare il minimo elemento dall'array non ordinato
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[min_idx] > arr[j]:
+                min_idx = j
