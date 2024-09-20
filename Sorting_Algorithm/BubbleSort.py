560d0eac39c970163281d391cafc525b30f33ff6
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-09-20 08:03:57 +0200

    Update BubbleSort.py

--- Sorting_Algorithm/BubbleSort.py
+++ Sorting_Algorithm/BubbleSort.py
@@ -1,1 +1,5 @@
+'''
+Funziona confrontando coppie di elementi adiacenti e scambiandoli se sono nell'ordine sbagliato. Questo processo si ripete fino a quando l'intero array non è ordinato. Anche se semplice, Bubble Sort è tra i più lenti.
+'''
+
 
