06c8917857318ac15bdb0a2e090acb4760e4d964
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-09-21 00:49:55 +0200

    Update BubbleSort.py

--- Sorting_Algorithm/BubbleSort.py
+++ Sorting_Algorithm/BubbleSort.py
@@ -2,4 +2,21 @@
 Funziona confrontando coppie di elementi adiacenti e scambiandoli se sono nell'ordine sbagliato. Questo processo si ripete fino a quando l'intero array non è ordinato. Anche se semplice, Bubble Sort è tra i più lenti.
 '''
 
+def bubble_sort(arr):
+    n = len(arr)
+
+    # Esegui il ciclo esterno n-1 volte
+    for i in range(n):
+        # Esegui il ciclo interno n-i-1 volte
+        for j in range(0, n-i-1):
+            # Confronta elementi adiacenti e scambiali se sono nell'ordine sbagliato
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+# Esempio di utilizzo
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+print("Array originale:", arr)
+bubble_sort(arr)
+print("Array ordinato:", arr)
 
