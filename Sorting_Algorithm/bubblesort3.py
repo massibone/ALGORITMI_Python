a30ac733105a84d520318ae48ec40c8d2e83fd9d
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-09-24 08:28:10 +0200

    Update bubblesort3.py

--- Sorting_Algorithm/bubblesort3.py
+++ Sorting_Algorithm/bubblesort3.py
@@ -1,1 +1,15 @@
 
+'''
+versione migliorata che riduce iterazioni
+'''
+def optimized_bubble_sort(arr, reverse=False):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if (reverse and arr[j] < arr[j+1]) or (not reverse and arr[j] > arr[j+1]):
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
