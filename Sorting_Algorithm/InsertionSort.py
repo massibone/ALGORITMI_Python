8c11360cfa88e26ed8e5f3387877032d11034825
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-09-29 02:40:26 +0200

    Update InsertionSort.py

--- Sorting_Algorithm/InsertionSort.py
+++ Sorting_Algorithm/InsertionSort.py
@@ -4,5 +4,6 @@ ordinata, spostando gli elementi più grandi
 di uno spazio in avanti per fare posto 
 al nuovo elemento. 
 È più efficiente di Bubble Sort e Selection Sort 
-per piccoli set di dati o liste già quasi ordinate nel caso peggiore.
+per piccoli set di dati o liste 
+già quasi ordinate nel caso peggiore.
 '''
