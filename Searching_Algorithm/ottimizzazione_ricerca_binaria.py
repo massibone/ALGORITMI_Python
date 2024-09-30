335423e89f09a514cf07abd175e2a65d1b441a27
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-09-30 08:08:19 +0200

    Update ottimizzazione_ricerca_binaria.py

--- Searching_Algorithm/ottimizzazione_ricerca_binaria.py
+++ Searching_Algorithm/ottimizzazione_ricerca_binaria.py
@@ -9,3 +9,21 @@ def ricerca_lineare(lista, elemento):
         if valore == elemento:
             return i
     return -1
+def ricerca_binaria(lista, elemento):
+    sinistra, destra = 0, len(lista) - 1
+    while sinistra <= destra:
+        medio = (sinistra + destra) // 2
+        if lista[medio] == elemento:
+            return medio
+        elif lista[medio] < elemento:
+            sinistra = medio + 1
+        else:
+            destra = medio - 1
+    return -1
+
+# Genera una lista grande e ordinata
+n = 1000000
+lista_grande_ordinata = genera_lista_ordinata(n)
+
+# Elemento da cercare
+elemento_da_cercare = random.choice(lista_grande_ordinata)
