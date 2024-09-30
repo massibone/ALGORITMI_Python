921bf0b5abb202dfb72659b41420e21b70b8d6cf
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-09-30 08:07:54 +0200

    Update ottimizzazione_ricerca_binaria.py

--- Searching_Algorithm/ottimizzazione_ricerca_binaria.py
+++ Searching_Algorithm/ottimizzazione_ricerca_binaria.py
@@ -1,1 +1,11 @@
+import random
+import time
 
+def genera_lista_ordinata(n):
+    return sorted([random.randint(1, 1000000) for _ in range(n)])
+
+def ricerca_lineare(lista, elemento):
+    for i, valore in enumerate(lista):
+        if valore == elemento:
+            return i
+    return -1
