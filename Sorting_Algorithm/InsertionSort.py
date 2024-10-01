9db95b8cbfcd6a1110caed846959363d0daa2afc
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-10-01 08:07:17 +0200

    Update InsertionSort.py

--- Sorting_Algorithm/InsertionSort.py
+++ Sorting_Algorithm/InsertionSort.py
@@ -7,3 +7,16 @@ al nuovo elemento.
 per piccoli set di dati o liste 
 già quasi ordinate nel caso peggiore.
 '''
+def insertion_sort(arr):
+    # Inizia dal secondo elemento (indice 1)
+    for i in range(1, len(arr)):
+        # Elemento da inserire
+        key = arr[i]
+        
+        # Sposta gli elementi di arr[0..i-1] che sono maggiori di key
+        # di una posizione avanti rispetto alla loro posizione attuale
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        
