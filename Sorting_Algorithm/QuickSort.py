08611d825e0f78bbc68059480c3021ea1e4be1d8
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-10-04 08:03:26 +0200

    Update QuickSort.py

--- Sorting_Algorithm/QuickSort.py
+++ Sorting_Algorithm/QuickSort.py
@@ -1,1 +1,5 @@
-
+'''
+Quick Sort è un algoritmo di ordinamento molto efficiente che segue il paradigma "divide et impera". 
+L'algoritmo sceglie un elemento come "pivot" e divide l'array in due parti: 
+gli elementi minori del pivot e quelli maggiori. Ordina poi ricorsivamente le due parti.
+'''
