684f2a8badfae1d294be0654caab26702410ae82
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-10-05 09:48:19 +0200

    Update QuickSort.py

--- Sorting_Algorithm/QuickSort.py
+++ Sorting_Algorithm/QuickSort.py
@@ -3,3 +3,11 @@ Quick Sort è un algoritmo di ordinamento molto efficiente che segue il paradigm
 L'algoritmo sceglie un elemento come "pivot" e divide l'array in due parti: 
 gli elementi minori del pivot e quelli maggiori. Ordina poi ricorsivamente le due parti.
 '''
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
