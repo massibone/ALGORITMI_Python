68ebe7e5da855ede20ee399b0945e07d1a560212
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-10-07 07:34:55 +0200

    Update HeapSort.py

--- Sorting_Algorithm/HeapSort.py
+++ Sorting_Algorithm/HeapSort.py
@@ -1,1 +1,3 @@
-
+'''
+Descrizione: Heap Sort si basa su una struttura dati chiamata "heap", che è un albero binario completo. L'algoritmo costruisce un max-heap (o min-heap) dall'array e poi estrae l'elemento massimo (o minimo), ricostruendo il heap finché l'array non è ordinato.
+'''
