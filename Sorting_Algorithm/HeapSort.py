340a7b8550a2de6660edaa6db3c4735d32d84ee0
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-10-08 08:04:28 +0200

    Update HeapSort.py

--- Sorting_Algorithm/HeapSort.py
+++ Sorting_Algorithm/HeapSort.py
@@ -4,6 +4,7 @@ che è un albero binario completo.
 L'algoritmo costruisce un max-heap 
 (o min-heap) dall'array e poi estrae 
 l'elemento massimo (o minimo), ricostruendo il heap finché l'array non è ordinato.
+
 '''
 def heapify(arr, n, i):
     largest = i
