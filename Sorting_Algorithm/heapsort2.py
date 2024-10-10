9103de3347e428850c13dc3df8c28cc430dfb340
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-10-10 08:05:51 +0200

    Update heapsort2.py

--- Sorting_Algorithm/heapsort2.py
+++ Sorting_Algorithm/heapsort2.py
@@ -11,3 +11,26 @@ def heapify(arr, n, i):
         left = 2 * i + 1
         right = 2 * i + 2
 
+        if left < n and arr[left] > arr[largest]:
+            largest = left
+        if right < n and arr[right] > arr[largest]:
+            largest = right
+        
+        if largest == i:
+            break
+        
+        arr[i], arr[largest] = arr[largest], arr[i]
+        i = largest
+
+def heap_sort(arr):
+    """
+    Ordina un array utilizzando l'algoritmo Heap Sort.
+    
+    :param arr: L'array da ordinare
+    :return: None (l'array viene ordinato in-place)
+    """
+    if not arr:
+        return  # Gestione del caso di array vuoto
+    
+    n = len(arr)
+    
