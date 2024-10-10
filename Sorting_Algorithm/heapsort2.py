6377ce1a05b37afac4456e155de3492f4c31d565
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-10-10 08:06:30 +0200

    Update heapsort2.py

--- Sorting_Algorithm/heapsort2.py
+++ Sorting_Algorithm/heapsort2.py
@@ -1,11 +1,11 @@
 def heapify(arr, n, i):
-    """
+    '''
     Mantiene la proprietà del max-heap per il sottoalbero radicato all'indice i.
     
     :param arr: L'array da heapify
     :param n: La dimensione del heap
     :param i: L'indice radice del sottoalbero
-    """
+    '''
     largest = i
     while True:
         left = 2 * i + 1
