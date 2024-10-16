9700f05c435ce6f34770debc2c40ad9f54c0eea9
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-10-16 08:38:12 +0200

    Update heapsort3.py

--- Sorting_Algorithm/heapsort3.py
+++ Sorting_Algorithm/heapsort3.py
@@ -1,3 +1,4 @@
 '''
-heap sort ha il vantaggio di essere un algoritmo in-place (non richiede spazio aggiuntivo) rispetto al Merge Sort, che richiede spazio aggiuntivo per le operazioni di fusione.
+heap sort ha il vantaggio di essere un algoritmo in-place (non richiede spazio aggiuntivo) 
+rispetto al Merge Sort, che richiede spazio aggiuntivo per le operazioni di fusione.
 '''
