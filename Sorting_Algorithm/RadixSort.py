1dc8466bfea573bb176eb203bc69bb67c43ab4d7
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-10-18 08:05:03 +0200

    Update RadixSort.py

--- Sorting_Algorithm/RadixSort.py
+++ Sorting_Algorithm/RadixSort.py
@@ -1,5 +1,6 @@
 '''
 Radix Sort è un algoritmo di ordinamento non comparativo che ordina gli elementi basandosi su ogni cifra individuale, partendo dalla cifra meno significativa fino a quella più significativa (o viceversa).
+La complessità temporale del Radix Sort è O(kn), dove k è il numero di cifre nel numero massimo.
 '''
 
 
