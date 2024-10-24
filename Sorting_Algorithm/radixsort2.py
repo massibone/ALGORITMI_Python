3d4f13c44c810686d92d20116768cee363965c76
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-10-24 08:04:16 +0200

    Update radixsort2.py

--- Sorting_Algorithm/radixsort2.py
+++ Sorting_Algorithm/radixsort2.py
@@ -3,4 +3,9 @@ def radix_sort(arr):
     if len(arr) <= 1:
         return arr
 
+        max_num = max(arr)
+    place_value = 1
+    while max_num // place_value > 0:
+        counting_sort(arr, place_value)
+        place_value *= 10
 
