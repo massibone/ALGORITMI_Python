688f0c0f4b9c4ae336b400dd9420a21c1aaaefa2
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-10-25 08:06:23 +0200

    Update radixsort2.py

--- Sorting_Algorithm/radixsort2.py
+++ Sorting_Algorithm/radixsort2.py
@@ -9,3 +9,15 @@ def radix_sort(arr):
         counting_sort(arr, place_value)
         place_value *= 10
 
+def counting_sort(arr, place_value):
+    """Funzione di supporto per il Radix Sort. Implementa l'Ordine per Conteggio basato sulla cifra corrente."""
+    n = len(arr)
+    output = [0] * n
+    count = [0] * 10
+
+    for num in arr:
+        index = (num // place_value) % 10
+        count[index] += 1
+
+    for i in range(1, 10):
+        count[i] += count[i - 1]
