eb951129d08c4e3b97b0f16bf4fd05a2a59acc36
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-11-01 07:18:55 +0100

    Update countingSort.py

--- Sorting_Algorithm/countingSort.py
+++ Sorting_Algorithm/countingSort.py
@@ -1,1 +1,7 @@
-
+def counting_sort(array):
+    """
+    Implementazione dell'algoritmo di Counting Sort per ordinare un array di numeri interi.
+    
+    :param array: Lista di numeri interi con un range limitato.
+    :return: Lista ordinata in senso crescente.
+    """
