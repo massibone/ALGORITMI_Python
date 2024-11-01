4d463fbd564e1dbe4100973860a9b2ffdadbc748
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-11-02 00:58:43 +0100

    Update countingSort.py

--- Sorting_Algorithm/countingSort.py
+++ Sorting_Algorithm/countingSort.py
@@ -5,3 +5,7 @@ def counting_sort(array):
     :param array: Lista di numeri interi con un range limitato.
     :return: Lista ordinata in senso crescente.
     """
+    # Passo 1: Trovare il valore massimo e minimo nell'array per determinare la dimensione dell'array di conteggi
+    min_value = min(array)
+    max_value = max(array)
+    
