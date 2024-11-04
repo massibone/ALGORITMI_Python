05451184f80f852fae129fa65d0238fb5efce1a7
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-11-04 08:05:42 +0100

    Update countingSort.py

--- Sorting_Algorithm/countingSort.py
+++ Sorting_Algorithm/countingSort.py
@@ -9,10 +9,9 @@ def counting_sort(array):
     min_value = min(array)
     max_value = max(array)
     
-  # Passo 2: Creare un array di conteggi, con dimensione pari alla differenza tra il valore massimo e minimo più uno
+    # Passo 2: Creare un array di conteggi, con dimensione pari alla differenza tra il valore massimo e minimo più uno
     count_array = [0] * (max_value - min_value + 1)
     
-
     # Passo 3: Contare le occorrenze di ogni valore nell'array originale
     for num in array:
         count_array[num - min_value] += 1
