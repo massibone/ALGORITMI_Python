ebc76e3441ad8393d6b8ec98362f3afdcbe692c8
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-11-07 08:05:19 +0100

    Update ShellSort.py

--- Sorting_Algorithm/ShellSort.py
+++ Sorting_Algorithm/ShellSort.py
@@ -1,5 +1,6 @@
 '''
-Shell Sort è una generalizzazione dell'insertion sort che permette agli elementi di essere scambiati tra loro a distanze maggiori, riducendo gradualmente queste distanze. Si basa su una sequenza di gap per migliorare l'efficienza rispetto all'Insertion Sort.
+Shell Sort è una generalizzazione dell'insertion sort che permette agli elementi di essere scambiati tra loro a distanze maggiori,
+riducendo gradualmente queste distanze. Si basa su una sequenza di gap per migliorare l'efficienza rispetto all'Insertion Sort.
 '''
 def shell_sort_shell_sequence(array):
     """
