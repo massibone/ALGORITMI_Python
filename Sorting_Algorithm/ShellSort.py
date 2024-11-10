eab47956c0c03803a03accd5b74266f2a32b88f0
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-11-10 08:52:10 +0100

    Update ShellSort.py

--- Sorting_Algorithm/ShellSort.py
+++ Sorting_Algorithm/ShellSort.py
@@ -5,7 +5,8 @@ agli elementi di essere scambiati
 tra loro a distanze maggiori,
 riducendo gradualmente queste distanze. 
 Si basa su una sequenza di gap per 
-migliorare l'efficienza rispetto all'Insertion Sort.
+migliorare l'efficienza rispetto 
+all'Insertion Sort.
 '''
 
 def shell_sort_shell_sequence(array):
