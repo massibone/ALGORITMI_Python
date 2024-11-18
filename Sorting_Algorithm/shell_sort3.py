5e02f5f5d2c4b7083769209d570549acef80e059
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-11-18 08:20:25 +0100

    Update shell_sort3.py

--- Sorting_Algorithm/shell_sort3.py
+++ Sorting_Algorithm/shell_sort3.py
@@ -7,7 +7,7 @@ def shell_sort_ciura_sequence(array):
     Implementazione di Shell Sort con la sequenza di gap migliorata di Ciura.
     :param array: Lista di elementi da ordinare.
     :return: Lista ordinata.
-    """
+    '''
     if len(array) <= 1:
         return array
 
