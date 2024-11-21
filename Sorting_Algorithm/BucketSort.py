4d5425c211f5f18805e5a2253bf4c9148e74bf14
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-11-21 08:02:23 +0100

    Update BucketSort.py

--- Sorting_Algorithm/BucketSort.py
+++ Sorting_Algorithm/BucketSort.py
@@ -1,3 +1,5 @@
 '''
-Bucket Sort divide l'array originale in un certo numero di "secchi" (bucket), e poi ordina ogni secchio individualmente usando un altro algoritmo di ordinamento (spesso Insertion Sort o Merge Sort). Infine, i secchi ordinati vengono combinati per ottenere l'array finale ordinato.
+Bucket Sort divide l'array originale in un certo numero di "secchi" (bucket), 
+e poi ordina ogni secchio individualmente usando un altro algoritmo di ordinamento (spesso Insertion Sort o Merge Sort). 
+Infine, i secchi ordinati vengono combinati per ottenere l'array finale ordinato.
 '''
