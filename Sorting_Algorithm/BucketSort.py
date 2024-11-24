78908c46136c2219e7e7628457b99720bbbe4bd2
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-11-24 05:54:30 +0100

    Update BucketSort.py

--- Sorting_Algorithm/BucketSort.py
+++ Sorting_Algorithm/BucketSort.py
@@ -47,3 +47,10 @@ def insertion_sort(arr):
   Args:
     arr: L'array da ordinare.
   """
+ for i in range(1, len(arr)):
+    key = arr[i]
+    j = i - 1
+    while j >= 0 and key < arr[j]:
+      arr[j + 1] = arr[j]
+      j -= 1
+    arr[j + 1] = key
