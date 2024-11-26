3dcfe6534dcf2cfc2296f1955adf1d5da46cb06d
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-11-26 08:07:09 +0100

    Update TimSort.py

--- Sorting_Algorithm/TimSort.py
+++ Sorting_Algorithm/TimSort.py
@@ -1,1 +1,2 @@
-
+"Tim Sort è un algoritmo ibrido, utilizzato in molte librerie standard (come Python). Combina le tecniche di Merge Sort e Insertion Sort per ottimizzare le prestazioni su dataset reali, in particolare quelli che hanno già porzioni di dati ordinati.
+'''
