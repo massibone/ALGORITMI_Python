a3bb833853fa97d105ad770b73aeded285055e30
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2024-11-27 08:27:04 +0100

    Update TimSort.py

--- Sorting_Algorithm/TimSort.py
+++ Sorting_Algorithm/TimSort.py
@@ -17,3 +17,42 @@ def insertion_sort(arr, left=0, right=None):
             j -= 1
 
         arr[j + 1] = key_item
+
+    return arr
+
+def merge(left, right):
+    if not left:
+        return right
+
+    if not right:
+        return left
+
+    if left[0] < right[0]:
+        return [left[0]] + merge(left[1:], right)
+
+    return [right[0]] + merge(left, right[1:])
+
+def timsort(arr):
+    min_run = 32
+    n = len(arr)
+
+    for i in range(0, n, min_run):
+        insertion_sort(arr, i, min((i + min_run - 1), (n - 1)))
+
+    size = min_run
+    while size < n:
+        for start in range(0, n, size * 2):
+            midpoint = start + size
+            end = min((start + size * 2 - 1), (n - 1))
+
+            merged_array = merge(
+                arr[start:midpoint], arr[midpoint:end + 1]
+            )
+
+            arr[start:start + len(merged_array)] = merged_array
+
+        size *= 2
+
+    return arr
+
+ 
