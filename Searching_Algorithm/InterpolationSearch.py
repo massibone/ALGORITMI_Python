e35814bc4ed541ee8f30703f3c3bcce16e8673f3
Massimo Bonechi <88546932+massibone@users.noreply.github.com>
2025-04-06 09:58:56 +0200

    Update InterpolationSearch.py

--- Searching_Algorithm/InterpolationSearch.py
+++ Searching_Algorithm/InterpolationSearch.py
@@ -2,6 +2,7 @@ def interpolation_search(arr, x):
     low = 0
     high = len(arr) - 1
     
+    
     while low <= high and arr[low] <= x <= arr[high]:
         # Avoid division by zero
         if arr[high] == arr[low]:
